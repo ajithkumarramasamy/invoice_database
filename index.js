a8cc3ec8b681aa8a811a250a8ba06d7a13c92d09
ajithkumarramasamy <ajithramasamy08042000@gmail.com>
2023-07-18 06:35:16 +0000

    invoice databasa

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
 
 let sql;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/", (request, response) => {
   response.send("Hollo");
 });
 
-app.listen(3000);
+app.listen(3001);
 const db = new sqlite3.Database(
   "./invoices.db",
   sqlite3.OPEN_READWRITE,
