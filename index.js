293aebfbed2db6cec96334146d1226489d40fc82
ajithkumarramasamy <ajithramasamy08042000@gmail.com>
2023-07-18 09:29:17 +0000

    Invoice

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.use(
   })
 );
 
+app.listen(3001);
+
+const db = new sqlite3.Database(
+  "./invoices.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.error(err.message);
+  }
+);
+
 app.get("/", (request, response) => {
   const {
     filterFinancialYear,
@@ -19,21 +29,12 @@ app.get("/", (request, response) => {
     filterToDate,
   } = request.query;
 
-  sql = `SELECT * FROM details WHERE FinancialYear = ${filterFinancialYear} AND InvoiceNumber = ${filterInvoiceNumber} AND InvoiceDate BETWEEN ${filterFromDate} AND ${filterToDate}`;
+  sql = `SELECT * FROM details`;
   db.all(sql, [], (err, rows) => {
     if (err) return console.error(err.message);
     response.send(rows);
   });
 });
-
-app.listen(3001);
-const db = new sqlite3.Database(
-  "./invoices.db",
-  sqlite3.OPEN_READWRITE,
-  (err) => {
-    if (err) return console.error(err.message);
-  }
-);
 
 //sql = `CREATE TABLE details(id INTEGER PRIMARY KEY AUTOINCREMENT,InvoiceDate TEXT NOT NULL,InvoiceNumber INTEGER NOT NULL,InvoiceAmount REAL NOT NULL,FinancialYear TEXT NOT NULL,UNIQUE (FinancialYear, InvoiceNumber))`;
 //db.run(sql);
